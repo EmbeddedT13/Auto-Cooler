6d50b75cf4b898b94d36cf0aa24583d5ded5d465
Ziad Ashraf Abdu Mohamed <ziad.mohamed04@eng-st.cu.edu.eg>
2026-04-27 21:35:05 +0300

    fix: generalize timer drivers
    - removed all hardcoded references to TIM3
    - updated API to take a  pointer to the specific Timer, accept the Channel (1 through 4) so you can drive up to 4 different fans or motors from a single timer.
    and accept PSC and ARR as arguments

--- mcal/timer/timer.c
+++ mcal/timer/timer.c
@@ -1,49 +1,70 @@
 #include "timer.h"
+#include "../rcc/rcc.h"
 
-void Timer3_PWM_Init(void) {
-    /* 1. Set the Timer Clock Frequency */
-    TIM3->PSC = 15;   /* Prescaler: 16MHz / (15 + 1) = 1MHz counter clock */
-    TIM3->ARR = 100;  /* Auto-Reload: 1MHz / 100 = 10kHz PWM frequency */
+void Timer_PWM_Init(timer* TIMx, uint8 Channel, uint16 PSC_Value, uint16 ARR_Value) {
+    /* 1. Power on the specific timer */
+    if(TIMx == TIM2)      { RCC_EnableTIM2(); }
+    else if(TIMx == TIM3) { RCC_EnableTIM3(); }
+    else if(TIMx == TIM4) { RCC_EnableTIM4(); }
+    /* Note: Add RCC_Enable macros to your rcc.h as needed! */
 
-    /* 2. Configure Channel 1 for PWM Mode 1 */
-    /* Clear Output Compare Mode bits (Bits 6:4) */
-    TIM3->CCMR1 &= ~(0x7 << 4);
-    
-    /* Set Output Compare Mode to PWM Mode 1 (Binary 110 = 0x6) */
-    TIM3->CCMR1 |= (0x6 << 4);
+    /* 2. Set Frequency */
+    TIMx->PSC = PSC_Value;
+    TIMx->ARR = ARR_Value;
 
-    /* Enable Output Compare Preload (Bit 3) */
-    SET_BIT(TIM3->CCMR1, 3);
-
-    /* 3. Enable the Capture/Compare output for Channel 1 */
-    SET_BIT(TIM3->CCER, 0);
-
-    /* 4. Initialize Duty Cycle to 0% (Fan Off) */
-    TIM3->CCR1 = 0;
-
-    /* 5. Enable the Timer Counter (Bit 0 of CR1) */
-    SET_BIT(TIM3->CR1, 0);
-}
-
-void Timer3_SetPWM_DutyCycle(uint8 duty_percent) {
-    /* Safety Cap: Ensure we don't overflow the 100% ARR limit */
-    if (duty_percent > 100) {
-        duty_percent = 100;
+    /* 3. Route the specific Channel */
+    switch(Channel) {
+        case TIMER_CH1:
+            TIMx->CCMR1 &= ~(0x7 << 4);   /* Clear mode */
+            TIMx->CCMR1 |=  (0x6 << 4);   /* PWM Mode 1 */
+            SET_BIT(TIMx->CCMR1, 3);      /* Preload Enable */
+            SET_BIT(TIMx->CCER, 0);       /* Output Enable */
+            TIMx->CCR1 = 0;               /* Start at 0 */
+            break;
+        case TIMER_CH2:
+            TIMx->CCMR1 &= ~(0x7 << 12);  
+            TIMx->CCMR1 |=  (0x6 << 12);  
+            SET_BIT(TIMx->CCMR1, 11);     
+            SET_BIT(TIMx->CCER, 4);       
+            TIMx->CCR2 = 0;
+            break;
+        case TIMER_CH3:
+            TIMx->CCMR2 &= ~(0x7 << 4);   
+            TIMx->CCMR2 |=  (0x6 << 4);   
+            SET_BIT(TIMx->CCMR2, 3);      
+            SET_BIT(TIMx->CCER, 8);       
+            TIMx->CCR3 = 0;
+            break;
+        case TIMER_CH4:
+            TIMx->CCMR2 &= ~(0x7 << 12);  
+            TIMx->CCMR2 |=  (0x6 << 12);  
+            SET_BIT(TIMx->CCMR2, 11);     
+            SET_BIT(TIMx->CCER, 12);      
+            TIMx->CCR4 = 0;
+            break;
     }
-    
-    /* Directly write the percentage to the Capture/Compare Register */
-    TIM3->CCR1 = duty_percent;
+
+    /* 4. Start the counter */
+    SET_BIT(TIMx->CR1, 0);
 }
 
+void Timer_SetPWM_Duty(timer* TIMx, uint8 Channel, uint32 Duty_Value) {
+    /* Safety check against ARR is done at the HAL level now */
+    switch(Channel) {
+        case TIMER_CH1: TIMx->CCR1 = Duty_Value; break;
+        case TIMER_CH2: TIMx->CCR2 = Duty_Value; break;
+        case TIMER_CH3: TIMx->CCR3 = Duty_Value; break;
+        case TIMER_CH4: TIMx->CCR4 = Duty_Value; break;
+    }
+}
+
+/* SysTick Delays remain exactly the same */
 void Timer_Delay_us(uint32 us) {
-    /* Assuming default internal 16 MHz clock. 1 microsecond = 16 ticks */
     SYSTICK_LOAD = (16 * us) - 1; 
-    SYSTICK_VAL = 0;                  /* Clear current timer value */
-    SYSTICK_CTRL = 5;                 /* Enable SysTick, use processor clock (Bits 0 and 2) */
-    
-    while ((SYSTICK_CTRL & (1 << 16)) == 0); /* Wait for the COUNTFLAG to turn to 1 */
-    
-    SYSTICK_CTRL = 0;                 /* Disable SysTick to save power */
+    SYSTICK_VAL = 0;                  
+    SYSTICK_CTRL = 5;                 
+    while ((SYSTICK_CTRL & (1 << 16)) == 0); 
+    SYSTICK_CTRL = 0;                 
 }
 
 void Timer_Delay_ms(uint32 ms) {
--- mcal/timer/timer.h
+++ mcal/timer/timer.h
@@ -4,39 +4,48 @@
 #include "../../core/STD_TYPES.h"
 #include "../../core/BIT_MATH.h"
 
-typedef struct
-{
-    volatile uint32 CR1;       /* Offset: 0x00 */
-    volatile uint32 CR2;       /* Offset: 0x04 */
-    volatile uint32 SMCR;      /* Offset: 0x08 */
-    volatile uint32 DIER;      /* Offset: 0x0C */
-    volatile uint32 SR;        /* Offset: 0x10 */
-    volatile uint32 EGR;       /* Offset: 0x14 */
-    volatile uint32 CCMR1;     /* Offset: 0x18 */
-    volatile uint32 CCMR2;     /* Offset: 0x1C */
-    volatile uint32 CCER;      /* Offset: 0x20 */
-    volatile uint32 CNT;       /* Offset: 0x24 */
-    volatile uint32 PSC;       /* Offset: 0x28 */
-    volatile uint32 ARR;       /* Offset: 0x2C */
-    volatile uint32 Reserved;  /* Offset: 0x30 */
-    volatile uint32 CCR1;      /* Offset: 0x34 */
-    volatile uint32 CCR2;      /* Offset: 0x38 */
-    volatile uint32 CCR3;      /* Offset: 0x3C */
-    volatile uint32 CCR4;      /* Offset: 0x40 */
-}timer;
+/* --- General Timer Register Map --- */
+typedef struct {
+    volatile uint32 CR1;       /* 0x00 */
+    volatile uint32 CR2;       /* 0x04 */
+    volatile uint32 SMCR;      /* 0x08 */
+    volatile uint32 DIER;      /* 0x0C */
+    volatile uint32 SR;        /* 0x10 */
+    volatile uint32 EGR;       /* 0x14 */
+    volatile uint32 CCMR1;     /* 0x18 */
+    volatile uint32 CCMR2;     /* 0x1C */
+    volatile uint32 CCER;      /* 0x20 */
+    volatile uint32 CNT;       /* 0x24 */
+    volatile uint32 PSC;       /* 0x28 */
+    volatile uint32 ARR;       /* 0x2C */
+    volatile uint32 Reserved;  /* 0x30 */
+    volatile uint32 CCR1;      /* 0x34 */
+    volatile uint32 CCR2;      /* 0x38 */
+    volatile uint32 CCR3;      /* 0x3C */
+    volatile uint32 CCR4;      /* 0x40 */
+} timer;
 
-/* Base Address */
-#define TIM3_BASE_ADD 0x40000400
-#define TIM3 ((timer*) TIM3_BASE_ADD)
+/* Hardware Base Addresses */
+#define TIM2 ((timer*) 0x40000000)
+#define TIM3 ((timer*) 0x40000400)
+#define TIM4 ((timer*) 0x40000800)
+#define TIM5 ((timer*) 0x40000C00)
 
-/* SysTick Registers (Hardcoded core addresses) */
+/* Channels */
+#define TIMER_CH1 1
+#define TIMER_CH2 2
+#define TIMER_CH3 3
+#define TIMER_CH4 4
+
+/* SysTick Registers */
 #define SYSTICK_CTRL  (*((volatile uint32*)0xE000E010))
 #define SYSTICK_LOAD  (*((volatile uint32*)0xE000E014))
 #define SYSTICK_VAL   (*((volatile uint32*)0xE000E018))
 
-/* Public APIs */
-void Timer3_PWM_Init(void);
-void Timer3_SetPWM_DutyCycle(uint8 duty_percent);
+/* --- Public APIs --- */
+void Timer_PWM_Init(timer* TIMx, uint8 Channel, uint16 PSC_Value, uint16 ARR_Value);
+void Timer_SetPWM_Duty(timer* TIMx, uint8 Channel, uint32 Duty_Value);
+
 void Timer_Delay_ms(uint32 ms);
 void Timer_Delay_us(uint32 us);
 
