6656bbfad6460d5c35ddb89d37476075e6de46d0
Ziad Ashraf Abdu Mohamed <ziad.mohamed04@eng-st.cu.edu.eg>
2026-04-28 11:32:05 +0300

    fix: update temp to use the new ADC driver

--- hal/temp/temp.c
+++ hal/temp/temp.c
@@ -1,19 +1,21 @@
 #include "temp.h"
 #include "../../mcal/adc/adc.h"
 
+static uint8 TempSensorChannel = 0;
 
 void TEMP_Init(uint8 Channel){
+    
+    TempSensorChannel = Channel;
 
-    ADC_Init(Channel, ADC_RESOLUTION_12B, ADC_CONTINUOUS); /* Initialize ADC for the requested channel, 12-bit resolution, Continuous Mode */
+    ADC_Init(Channel, ADC_RESOLUTION_12B, ADC_DISCONTINUOUS); /* Initialize ADC for the requested channel, 12-bit resolution, Continuous Mode */
 
-    ADC_Start(); /* Power on the ADC and trigger the first conversion */
 }
 
 
 uint16 TEMP_GetCelsius_x10(void){
 
-    uint16 adc_value = ADC_Read(); /* Fetch the latest autonomous 12-bit reading */
-
+    uint16 adc_value = ADC_ReadChannel(TempSensorChannel);
+    
     uint16 temp_x10 = (uint16) (((uint32) adc_value * TEMP_VOLTAGE) / TEMP_RESOLUTION); /* Calculate Temperature x 10 using fixed-point math to avoid floats */
 
     return temp_x10;
